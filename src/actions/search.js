27a58c2f792131feb2c1926aae9dd780cf963206
MikeQVo <angelictide@gmail.com>
2019-04-24 14:23:48 -0600

    completed majority tasks and working on containers

--- src/actions/search.js
+++ src/actions/search.js
@@ -5,8 +5,76 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
- 
-  //TODO:  Write an asynchronous action to handle a video search!
-};
 
+  //TODO:  Write an asynchronous action to handle a video search!
+
+  return () => {
+
+    //onchange -- have a function to handle onchange
+    //pull something out of Store to handle video
+  }
+};
 export default handleVideoSearch;
+/*
+var searchYouTube = ({key, query, max = 5}, callback) => {
+  $.get('https://www.googleapis.com/youtube/v3/search', {
+    part: 'snippet',
+    key: key,
+    q: query,
+    maxResults: max,
+    type: 'video',
+    videoEmbeddable: 'true'
+  })
+    .done(({items}) => {
+      if (callback) {
+        callback(items);
+      }
+    })
+    .fail(({responseJSON}) => {
+      responseJSON.error.errors.forEach((err) =>
+        console.error(err)
+      );
+    });
+};
+*/
+/*
+const mapDispatchToProps = dispatch => {
+  return {
+    var options = {
+      key: YOUTUBE_API_KEY,
+      query: q
+    };
+
+
+
+    onAddTodo: todo => {
+      dispatch(addTodo(toto));
+    }
+  };
+};
+*/
+/*
+searchYouTube(options,(data)=>{
+      console.log(data);
+      this.setState({
+        currentVideo: data.items[0],
+        videoList: data.items
+      });
+    });
+*/
+
+/*     return (dispatch) => {
+        setTimeout(() => {
+            // This function is able to dispatch other action creators
+            dispatch(itemsHasErrored(true));
+        }, 5000);
+*/
+/*
+var changeVideoList = (videos) => ({
+  //TODO:  Return some action object to change the list of videos in VideoList.
+  type: "CHANGE_VIDEO_LIST",
+  videos: videos
+});
+*/
+    //some logic here
+    //  dispatch(changeVideo(videos[0]));
